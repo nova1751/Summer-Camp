c14c012d558e04b1b5d3cffc1f5d9f3e3767fa95
nova1751 <nova2319427728@outlook.com>
2023-07-13 21:33:00 +0800

    msg update

--- week-01/7-11/demo/scripts/index.js
+++ week-01/7-11/demo/scripts/index.js
@@ -90,3 +90,98 @@ resizeY.addEventListener("mousedown", (e) => {
     document.addEventListener("mousemove", mouseMoveY);
     document.addEventListener("mouseup", mouseUpY);
 });
+// 获取元素，并指定它的类型为 HTMLDivElement
+const editor = document.getElementById("editor");
+const msg = document.querySelector("#msg-list");
+const sendMsg = document.querySelector("#sendMsg");
+const upload = document.querySelector("#upload");
+const imgUp1 = document.querySelector("#imgUp");
+// // 给元素添加一个 keydown 事件监听器，指定事件类型为 KeyboardEvent
+editor.addEventListener("keydown", (e) => {
+    // e 是一个 KeyboardEvent 对象，包含了按键的信息
+    // e.keyCode 是一个数字，表示按下的键的编码
+    // e.ctrlKey, e.shiftKey, e.altKey 是布尔值，表示是否同时按下了 Ctrl, Shift, Alt 键
+    if (e.key === "Enter") {
+        // 13 是 Enter 键的编码
+        if (e.ctrlKey) {
+            // 如果同时按下了 Ctrl 键
+            // 在光标处插入一个换行符
+            let br = document.createElement("br");
+            let selection = window.getSelection();
+            if (selection && selection.rangeCount > 0) {
+                let range = selection.getRangeAt(0);
+                range.insertNode(br);
+                // 将光标移动到换行符后面
+                range.setStartAfter(br);
+                range.setEndAfter(br);
+                selection.removeAllRanges();
+                selection.addRange(range);
+            }
+            console.log("test1");
+            // 阻止默认行为，即不在 div 外面插入一个换行符
+            e.preventDefault();
+        }
+        else if (e.shiftKey || e.altKey) {
+            // 如果同时按下了 Shift 或 Alt 键
+            // 不执行任何操作
+            e.preventDefault();
+        }
+        else {
+            // 如果只按下了 Enter 键
+            // 清空 div 内容
+            let div = document.createElement("div");
+            div.className = "msg-right";
+            let template = `             
+      <div class="bubble">
+        <div class="text">${editor.innerHTML}</div>
+      </div>
+      <div class="avatar">
+        <img src="./images/noa.png" alt="" srcset="" />
+      </div>
+   `;
+            div.innerHTML = template;
+            console.log(div);
+            msg.appendChild(div);
+            editor.innerHTML = "";
+            // 阻止默认行为，即不在 div 外面插入一个换行符
+            e.preventDefault();
+            msg.scrollTop = msg.scrollHeight;
+        }
+    }
+});
+sendMsg.addEventListener("click", () => {
+    let div = document.createElement("div");
+    div.className = "msg-right";
+    let template = `             
+  <div class="bubble">
+    <div class="text">${editor.innerHTML}</div>
+  </div>
+  <div class="avatar">
+    <img src="./images/noa.png" alt="" srcset="" />
+  </div>
+`;
+    div.innerHTML = template;
+    msg.appendChild(div);
+    editor.innerHTML = "";
+    msg.scrollTop = msg.scrollHeight;
+});
+imgUp1.addEventListener("click", () => {
+    upload.click();
+});
+upload.addEventListener("change", () => {
+    const src = URL.createObjectURL(upload.files[0]);
+    let div = document.createElement("div");
+    div.className = "msg-right";
+    let template = `             
+  <div class="bubble">
+    <div class="text"><img src="${src}" alt="" srcset="" /></div>
+  </div>
+  <div class="avatar">
+    <img src="./images/noa.png" alt="" srcset="" />
+  </div>
+`;
+    div.innerHTML = template;
+    msg.appendChild(div);
+    upload.value = "";
+    setTimeout(() => (msg.scrollTop = msg.scrollHeight), 100);
+});
