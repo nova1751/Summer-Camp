54e567c84b128dc4a1a906887900eeb78d98ea95
nova1751 <nova2319427728@outlook.com>
2023-07-21 00:06:46 +0800

    jsx finished

--- src/virtual-dom/src/models/jsx.ts
+++ src/virtual-dom/src/models/jsx.ts
@@ -9,94 +9,125 @@ const createElement = (
 };
 
 // ⽤于解析jsx字符串的函数
-// function parser(element: string, propNameV: string) {
-//   // ⽤正则表达式匹配标签名，属性和内容
-//   const tagRegex = /<(\w+)(.*?)>/;
-//   const propRegex = /(\w+)=(['"{])(.*?)(['"}])/g;
-//   const contentRegex = />(.*?)</;
+// ⽤于递归解析jsx字符串的函数
+function parser(element: string, variables: Props): VNode {
+  element = element
+    .replace(/[\r\n\t]/g, " ")
+    .replace(/\s+/g, " ")
+    .trim();
+  // ⽤正则表达式匹配标签名，属性和内容，贪婪匹配
+  const tagRegex = /<(\w+)(.*?)>([\s\S]*)<\/\1>/;
+  const propRegex = /(\w+)=(['"{])(.*?)(['"}])/g;
 
-//   // 获取标签名，属性和内容
-//   const tagMatch = element.match(tagRegex)!;
-//   console.log(tagMatch);
+  // 获取标签名，属性和内容
+  const tagMatch = element.match(tagRegex)!;
+  // console.log(element, tagMatch);
 
-//   const tagName = tagMatch[1];
-//   const propString = tagMatch[2];
-//   const contentMatch = element.match(contentRegex)!;
-//   const content = contentMatch[1];
-//   //   console.log(propString, propRegex.exec(propString));
+  const tagName = tagMatch[1];
+  const propString = tagMatch[2];
+  let content = tagMatch[3].trim();
 
-//   // 创建一个空对象存储属性
-//   const props: Props = {};
+  // console.log(tagMatch);
 
-//   // 遍历属性字符串，获取属性名和值
-//   let propMatch;
-//   while ((propMatch = propRegex.exec(propString)) !== null) {
-//     const propName = propMatch[1];
-//     const propValue = propMatch[3];
-//     // 如果属性值是一个变量，就从当前作⽤域中获取它的值
-//     console.log(propMatch);
+  // 创建一个空对象存储属性
+  const props: Props = {};
 
-//     if (propMatch[2] === "{" && propMatch[4] === "}") {
-//       props[propName] = propNameV;
-//     } else {
-//       // 否则就直接赋值
-//       props[propName] = propValue;
-//     }
-//   }
-
-//   // 返回构造的VDOM对象
-//   return createElement(tagName, props, [content]);
-// }
-function parser(element: string): VNode | null {
-  const regex = /<(\w+)\s*([^>]*)>(.*?)<\/\1>/s;
-  const match = element.match(regex);
-  console.log(match);
-
-  if (match) {
-    const [, tagName, attributes, innerHTML] = match;
-    const props: { [key: string]: any } = {};
-    const propsRegex = /\s*([\w-]+)="([^"]*)"/g;
-
-    let propMatch;
-    while ((propMatch = propsRegex.exec(attributes))) {
-      const [, propName, propValue] = propMatch;
+  // 遍历属性字符串，获取属性名和值
+  let propMatch;
+  while ((propMatch = propRegex.exec(propString)) !== null) {
+    const propName = propMatch[1];
+    const propValue = propMatch[3];
+    // 如果属性值是一个变量，就从当前作⽤域中获取它的值
+    if (propMatch[2] === "{" && propMatch[4] === "}") {
+      props[propName] = variables[propValue];
+    } else {
+      // 否则就直接赋值
       props[propName] = propValue;
     }
+  }
+  // Recursively parse nested tags in the content
+  const children: Child[] = [];
+  // const contentRegex = /(<(\w+)(.*?)>([\s\S]*?)<\/\2>)|([^<>]+)/g;
+  // const contentRegex = /(<(\w+)(.*?)>([\s\S]*?)<\/\2>)|([^<>]+)/g;
+  // let contentMatch;
+  // while ((contentMatch = contentRegex.exec(content)) !== null) {
+  //   // console.log(contentMatch);
 
-    const children: Child[] = [];
+  //   const mixedMatch = contentMatch[0].trim();
+  //   if (contentMatch[1]) {
+  //     children.push(parser(mixedMatch, variables));
+  //   }
+  //   if (contentMatch[5]) {
+  //     children.push(mixedMatch);
+  //   }
+  // }
+  // 判断内容是否包含嵌套的标签
+  console.log(content);
 
-    if (innerHTML) {
-      // Recursively parse nested elements
-      const nestedElements = parser(innerHTML);
-      console.log(innerHTML, nestedElements);
-
-      if (Array.isArray(nestedElements)) {
-        children.push(...nestedElements);
-      } else {
-        children.push(nestedElements as VNode);
+  if (/<\w+/.test(content)) {
+    // 如果包含，就递归地解析每个子标签，并将其添加到子节点数组中
+    let subElement = "";
+    let level = 0;
+    for (let i = 0; i < content.length; i++) {
+      subElement += content[i];
+      if (content[i + 1] === "<" && content[i + 2] !== "/") {
+        if (level === 0) {
+          // 添加非空判断
+          if (subElement.trim()) {
+            children.push(subElement.trim());
+          }
+          subElement = "";
+        }
+        level++;
+      }
+      if (content[i + 1] === "<" && content[i + 2] === "/") {
+        level--;
+      }
+      if (level === 0 && content[i] === ">") {
+        children.push(parser(subElement.trim(), variables));
+        subElement = "";
       }
     }
-
-    return createElement(tagName, props, children);
+  } else {
+    // 如果不包含，就直接将内容添加到子节点数组中
+    if (content.trim()) {
+      children.push(content.trim());
+    }
   }
-
-  return null;
+  // 返回构造的VDOM对象
+  return createElement(tagName, props, children);
 }
 export const test = () => {
-  //   const propName = "title";
-  //   const title = "<h1 className={propName}>Hello, world!</h1>";
-  //   const vdom = parser(title, propName);
-  //   const vNode = createElement("h1", { className: "title" }, ["Hello, world!"]);
-  //   console.log(
-  //     vdom,
-  //     JSON.stringify(vdom),
-  //     JSON.stringify(vdom) == JSON.stringify(vNode)
-  //   );
-  const propName = "title";
-  const title = "<h1 className={propName}>Hello, world!</h1>";
-  const paragraph = "<p>This is a paragraph.</p>";
-  const nestedElement = `<div>${title}${paragraph}</div>`;
-  const vdom = parser(nestedElement);
+  const variables = {
+    propName: "title",
+    app: "app111",
+    ccc: "11233ss",
+  };
+  let title = `
+  <h1 className={propName}>
+    Hello, world!
+    <div id={app}>
+      test
+      <p id="app">test</p>
+      <div id={app}>
+        test
+        <p id="app">test</p>
+      </div>
+    </div>
+    ss
+    <div id={ccc}>
+      test  
+    </div>
+    test
+  </h1>`;
 
-  //   console.log(vdom);
+  const vdom = parser(title, variables);
+  // console.log(title);
+
+  const vNode = createElement("h1", { className: "title" }, ["Hello, world!"]);
+  console.log(
+    vdom,
+    JSON.stringify(vdom),
+    JSON.stringify(vdom) == JSON.stringify(vNode)
+  );
 };
